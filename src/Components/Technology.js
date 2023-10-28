3385f4791013e8fd0055a47a20acb144d677e5e2
Vinu Suhas <vinusuhas2000@gmail.com>
2023-10-28 22:05:41 +0530

    updated

--- src/Components/Technology.js
+++ src/Components/Technology.js
@@ -28,6 +28,8 @@ export function  Technology(){
             <div className="rightContainer">
             <h1>Top posts</h1>
             <hr style={{ border:"5px solid green"}}/>
+            <img src="https://shorturl.at/achz4"  style={{width:"400px",display:"block",borderRadius:"16px"}} />
+            <h2 style={{width:"400px"}}>Catch hot Ballons with adventures trekking</h2>
             <img src="https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/c0a3fdfc-8a4f-4b20-92ef-896499c05843.gif" height="519px" width="452px"/>
             </div>
             </div>
